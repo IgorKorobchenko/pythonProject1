444fada8b33e8d07210c2ea98f991c6eae7a4706
IgorKorobchenko <korobchenko.igor@gmail.com>
2022-09-27 09:04:41 -0700

    Initial commit

---
+++ Lesson1.py
@@ -0,0 +1,57 @@
+"""user_name = input("Enter your name:")
+age = int(input('Enter your year of bith'))
+current_age =2022 - age
+message = "Helo, "
+print(f'{message} {user_name} your age is: {current_age}') #для вывода переменных одной строкой"""
+#Home work
+# print("""  *****
+# *       *
+# *       *
+#   *****""")
+# print (" *****")
+# print ("*     *")
+# print ("*     *")
+# print (" *****")
+
+
+
+# user_name = input("Please insert your name:")
+# print (f'Hello {user_name} !')
+
+# a = 5.33
+# b = str(a)
+#
+# print (round(a))
+# print('Don\'t touch')
+# e = input("User name:")
+# n = int(input("Age:"))
+# print("age = " + str(n))
+#
+# print(f'User name = {e} and Age = {n}')
+
+#m = input("Digit and string:")
+#print(m)
+#m = m.isdigit()
+
+# star = "#"
+# space = " "
+# print(star*9 + ("\n" + star + space*7 + star)*2 + "\n" + star*9)
+
+# T = 1,2,3,4
+# print(T)
+# print(T[0], T[1], T[2])
+# print(*T, sep= ":", end="!\n")
+#
+# def hello_name(name: str, n:int):
+#     for i in range (n):
+#         print("Hi",name)
+
+# A = (1,6,3,5)
+# # print(*A)
+# for x in A:
+#     print(x)
+
+# A = [(1, 10), (2, 20), (3, 30)]
+# for i in range(len(A)):
+#     print(A)
+
